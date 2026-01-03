e7ecb9b48bf1f102cee33867d629c6cdd157a903
anjalinaragude <naragudeav@gmail.com>
2026-01-03 15:06:18 +0530

    add changes

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state"
+    bucket = "young-minds-app-project-terraform-state144"
     key = "main"
-    region = "us-east-1"
+    region = "ap-northeast-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
